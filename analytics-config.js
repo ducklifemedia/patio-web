398da774d177102e3cfe7577d7bae56d85d7d26a
ducklifemedia <daniel@ducklifemedia.com>
2026-09-21 05:41:20 -0300

    Connect PATIO Meta Pixel and weekly editorial campaign tags

--- analytics-config.js
+++ analytics-config.js
@@ -1,7 +1,7 @@
-/* ERP-030: GA4 creado por T4; recepción pendiente. Pixel todavía no disponible. */
+/* ERP-030: GA4 y Pixel reales creados por T4. Recepción en UI pendiente de verificar. */
 window.PATIO_ANALYTICS_CONFIG = Object.freeze({
   ga4MeasurementId: 'G-WV2QY9NT1B',
-  metaPixelId: '',
+  metaPixelId: '1115846814301499',
   // Para otra marca, empezar con [] y agregar sólo el origen HTTPS autorizado.
   productionOrigins: ['https://red-cliff-062c48b0f.4.azurestaticapps.net'],
   debug: false,
@@ -12,7 +12,7 @@ window.PATIO_ANALYTICS_CONFIG = Object.freeze({
   allowedUtm: {
     utm_source: ['instagram', 'facebook', 'meta'],
     utm_medium: ['social', 'organic_social', 'paid_social'],
-    utm_campaign: ['patio_lanzamiento', 'patio-lanzamiento', 'patio-diario', 'patio-coleccion'],
-    utm_content: ['ad01_home', 'ad02_blog', 'ad03_bruma', 'home', 'blog', 'productos', 'balcon-pequeno', 'capas-de-luz', 'texturas-afuera', 'dia-1', 'dia-2', 'dia-3', 'dia-4', 'dia-5', 'dia-6', 'dia-7']
+    utm_campaign: ['patio_lanzamiento', 'patio_semana_01', 'patio-lanzamiento', 'patio-diario', 'patio-coleccion'],
+    utm_content: ['ad01_home', 'ad02_blog', 'ad03_bruma', 'w1d1_afuera', 'w1d2_guia', 'w1d3_bruma', 'w1d4_paleta', 'w1d5_luz', 'w1d6_cuidado', 'w1d7_ritual', 'home', 'blog', 'productos', 'balcon-pequeno', 'capas-de-luz', 'texturas-afuera', 'dia-1', 'dia-2', 'dia-3', 'dia-4', 'dia-5', 'dia-6', 'dia-7']
   }
 });
